d1e5a5efb7d96e513a4fddf1b15696742adf6e3a
kalonzo <kalonzo@bluewin.ch>
2019-04-15 09:48:17 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+
+import {HttpModule} from '@angular/http';
+import {MatInputModule} from '@angular/material';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { FormsModule } from '@angular/forms';
+
+const appRoutes: Routes = [
+   {
+      path: '',
+      component: UserloginComponent
+   },
+   {
+      path: 'app-mainpage',
+      component: MainpageComponent
+   }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      UserloginComponent,
+      MainpageComponent,
+      AppareilComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule,
+      HttpModule,
+      MatTableModule,
+      MatInputModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
